bf76435fe9cb4d27d899178d41a630c8c72ca3c2
nazim <amin.nazim13@gmail.com>
2015-10-27 15:36:58 -0400

    initial setup .. yo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ndtndtApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ndtndtApp
+ */
+angular.module('ndtndtApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
