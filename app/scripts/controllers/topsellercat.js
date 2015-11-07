862c4e328a8f10d921694480ae000565e0969e38
nazim <amin.nazim13@gmail.com>
2015-11-07 00:29:05 -0500

    midnight cookie

--- app/scripts/controllers/topsellercat.js
+++ app/scripts/controllers/topsellercat.js
@@ -8,10 +8,32 @@
  * Controller of the ndtndtApp
  */
 angular.module('ndtndtApp')
-  .controller('TopsellercatCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('TopsellercatCtrl', function ($scope) {
+        $scope.topsellercategory = [{
+                "title": "Laptops",
+                "url": "http://ndtndt.com/url",
+                "image": "../images/bike.png"
+}, {
+                "title": "Kitchen",
+                "url": "http://ndtndt.com/url",
+                "image": "../images/bike.png"
+},
+            {
+                "title": "Toys",
+                "url": "http://ndtndt.com/url",
+                "image": "../images/bike.png"
+                                   },
+            {
+                "title": "Clothing",
+                "url": "http://ndtndt.com/url",
+                "image": "../images/bike.png"
+                                   }, {
+                "title": "Shoes",
+                "url": "http://ndtndt.com/url",
+                "image": "../images/bike.png"
+                                   }, {
+                "title": "Cars",
+                "url": "http://ndtndt.com/url",
+                "image": "../images/bike.png"
+                                   }];
+    });
