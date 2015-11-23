fa8a325fa07db29f3030d5906dab36c72d00b3f9
nazim <amin.nazim13@gmail.com>
2015-11-23 16:30:13 -0500

    sortBy - done.

--- app/scripts/controllers/products.js
+++ app/scripts/controllers/products.js
@@ -10,318 +10,96 @@
 angular.module('ndtndtApp')
   .controller('ProductsCtrl', function($scope) {
     this.items = geta();
+
+    $scope.filter = {};
+
+    $scope.getCat = function() {
+      return (items || []).map(function(item) {
+        return item.itemtype;
+      }).filter(function(item, index, mappedArray) {
+        return mappedArray.indexOf(item) === index;
+      });
+    };
+
+
+    $scope.orderByPrice = function(item) {
+      if ($scope.orderBy == 'price-high-low') {
+        return -parseFloat(item.currentbid);
+      }
+      if ($scope.orderBy == 'price-low-high') {
+        return parseFloat(item.currentbid);
+      }
+    };
+    $scope.orderByBids = function(item) {
+      if ($scope.orderBy == 'bids-high-low') {
+        return -parseFloat(item.totalbids);
+      }
+      if ($scope.orderBy == 'bids-low-high') {
+        return parseFloat(item.totalbids);
+      }
+    };
+    $scope.orderByYear = function(item) {
+      if ($scope.orderBy == 'year-high-low') {
+        return -parseInt(item.yearmanufactured);
+      }
+      if ($scope.orderBy == 'year-low-high') {
+        return parseInt(item.yearmanufactured);
+      }
+    };
+    $scope.orderByPost = function(item) {
+      if ($scope.orderBy == 'post-high-low') {
+        return -item.postdate;
+      }
+      if ($scope.orderBy == 'post-low-high') {
+        return item.postdate;
+      }
+    };
+    $scope.orderByExp = function(item) {
+      if ($scope.orderBy == 'exp-high-low') {
+        return -item.expiredates;
+      }
+      if ($scope.orderBy == 'exp-low-high') {
+        return item.expiredates;
+      }
+    };
   });
 
-function colorTiles() {
-  var auctions = geta();
-  var tiles = [];
-  for (var i = 0; i <auctions.length; i++) {
-    tiles.push({
-      title: auctions[i].itemname,
-      icon: '../images/bike.png',
-      color: 'red',
-      colspan: getSize(i).col,
-      rowspan: getSize(i).row
-    });
-  }
-  return tiles;
-}
-
-function getSize(i) {
-  if (((i + 1) % 10) == 1 || ((i + 1) % 10) == 5) {
-    if (i != 10) {
-      return {
-        'col': 2,
-        'row': 2
-      }
-    }else{
-      return {
-        'col': 1,
-        'row': 1
-      }
-    }
-  } else if (((i + 1) % 10) == 4) {
-    return {
-      'col': 2,
-      'row': 1
-    }
-  } if(i!=0 && i%11==0) {
-    return {
-      'col': 1,
-      'row': 1
-    }
-  }else{
-    return {
-      'col': 1,
-      'row': 1
-    }
-  }
-}
-
-function geta (){
+function geta() {
   return [{
-        "auctionid": "2",
-        "itemname": "Nissan Sentra",
-        "openbid": "100.0000",
-        "bidincrement": "10.0000",
-        "currentbid": "120.0000",
-        "sellerid": "john",
-        "itemtype": "Car",
-        "yearmanufactured": "2007",
-        "postdate": "2015-11-17T01:25:41.227",
-        "expiredates": "2008-12-16T13:00:00"
-      }, {
-        "auctionid": "3",
-        "itemname": "Nissan Sentra",
-        "openbid": "100.0000",
-        "bidincrement": "10.0000",
-        "currentbid": "20.0000",
-        "sellerid": "john",
-        "itemtype": "Car",
-        "yearmanufactured": "2007",
-        "postdate": "2015-11-18T04:36:54.243",
-        "expiredates": "2011-12-16T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }, {
-        "auctionid": "1",
-        "itemname": "Titanic",
-        "openbid": "15.0000",
-        "bidincrement": "1.0000",
-        "currentbid": "500.0000",
-        "sellerid": "phil",
-        "itemtype": "DVD",
-        "yearmanufactured": "2005",
-        "postdate": "2015-11-17T01:25:41.220",
-        "expiredates": "2012-12-12T13:00:00"
-      }]
+    "auctionid": "2",
+    "itemname": "Nissan Sentra",
+    "openbid": "100.0000",
+    "bidincrement": "10.0000",
+    "currentbid": "200.00",
+    "sellerid": "john",
+    "itemtype": "Car",
+    "yearmanufactured": "2008",
+    "postdate": "2015-11-17T01:25:41.227",
+    "expiredates": "2016-11-17T01:25:41.227",
+    "totalbids": "30"
+  }, {
+    "auctionid": "3",
+    "itemname": "Something",
+    "openbid": "100.0000",
+    "bidincrement": "10.0000",
+    "currentbid": "400.00",
+    "sellerid": "john",
+    "itemtype": "Car",
+    "yearmanufactured": "2007",
+    "postdate": "2015-12-18T04:36:54.243",
+    "expiredates": "2017-11-17T01:25:41.227",
+    "totalbids": "20"
+  }, {
+    "auctionid": "3",
+    "itemname": "Something",
+    "openbid": "100.0000",
+    "bidincrement": "10.0000",
+    "currentbid": "350.00",
+    "sellerid": "john",
+    "itemtype": "Car",
+    "yearmanufactured": "2007",
+    "postdate": "2015-12-18T04:36:54.243",
+    "expiredates": "2017-11-17T01:25:41.227",
+    "totalbids": "15"
+  }]
 }
